c133782b8c8f05019157386d12953b41168543fe
brayanrodbajo <bryan951112@gmail.com>
2015-11-17 23:13:34 -0500

    cambio de nombres de archivos y creacion de views

--- SICON/administrador/forms.py
+++ SICON/administrador/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Repuesto
+from forms_vehiculos import *
 
 class RepuestoForm(ModelForm):
 	class Meta:
---
+++ SICON/administrador/views_vehiculos.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .forms import VehiculoNuevoForm, VehiculoNuevoForm
+from django.http import HttpResponseRedirect
+from .models import VehiculoNuevo,VehiculoUsado
+
+def crear_vehiculo_nuevo(request):
+    vehiculo_n = (VehiculoNuevo)
+    exito = False
+    if request.method=='POST':
+        vehiculo_n = VehiculoNuevoForm(request.POST)
+    if vehiculo_n.is_valid():
+        vehiculo_n.save()
+        exito = True
+        vehiculo_n= VehiculoNuevoForm()
+        return HttpResponseRedirect('/vehiculos_nuevos/')
+    return render(request, 'crear_vehiculo_nuevo.html', {'form':vehiculo_n,'exito':exito} )

d4a4d30ecc652bd0762d882935e71bab28ae333c
fernandosanchez1128 <fernandosanchez1128@gmail.com>
2015-12-03 00:15:00 -0500

    creacion de las clases para el modelo y el form para hacer integracion.

--- SICON/administrador/models.py
+++ SICON/administrador/models.py
@@ -1,15 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Repuesto (models.Model):
-	codigo = models.CharField(max_length = 40, unique=True, primary_key=True)
-	nombre = models.CharField(max_length = 40, unique=True)
-	marca  = models.CharField(max_length = 40)
-	costo  = models.IntegerField(blank=True, null=True)
-	marca_carro = models.CharField(max_length = 40)
-	modelo_carro = models.CharField(max_length = 40)
-	cantidad = models.IntegerField()
-	
+
+from .repuesto import Repuesto
 	
 	
 	
---
+++ SICON/administrador/repuesto.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Repuesto (models.Model):
+	codigo = models.CharField(max_length = 40, unique=True)
+	nombre = models.CharField(max_length = 40, unique=True)
+	marca  = models.CharField(max_length = 40)
+	costo  = models.IntegerField(blank=True, null=True)
+	marca_carro = models.CharField(max_length = 40)
+	modelo_carro = models.CharField(max_length = 40)
+	cantidad = models.IntegerField()

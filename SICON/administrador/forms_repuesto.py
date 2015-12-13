c41b4f23f018b6a6454bcff55f3574db675de3c5
fernandosanchez1128 <fernandosanchez1128@gmail.com>
2015-12-13 16:03:35 -0500

    .pyc

--- SICON/administrador/forms_repuesto.py
+++ SICON/administrador/forms_repuesto.py
@@ -1,17 +1,19 @@
+# -*- coding: utf-8 -*-
 from django.forms import ModelForm
 from django import forms
 from .models import Repuesto
-
 class RepuestoForm(ModelForm):
+	
 	class Meta:
+		
 		model = Repuesto
 		fields = ['codigo','nombre', 'marca', 'costo','marca_carro','modelo_carro','cantidad']
 		widgets = {
-		'codigo': forms.TextInput(attrs={'class':'form-control required','placeholder':'codigo'}),
+		'codigo': forms.TextInput(attrs={'class':'form-control required','placeholder':'Código'}),
 		'nombre': forms.TextInput(attrs={'class':'form-control','placeholder':'Nombre'}),
 		'marca': forms.TextInput(attrs={'class':'form-control','placeholder':'Marca'}),
 		'costo': forms.NumberInput(attrs={'class':'form-control','placeholder':'Costo'}),
 		'marca_carro': forms.Select(attrs={'class':'form-control','placeholder':'Marca del carro'}),
-		'modelo_carro': forms.Select(attrs={'class':'form-control','placeholder':'Modelo del carro'}),
+		'modelo_carro': forms.Select(attrs={'class':'form-control','placeholder':'Linea del carro'}),
 		'cantidad': forms.NumberInput(attrs={'class':'form-control','placeholder':'Cantidad inicial'}),
 	}

9f052df43cd70b9eff7105a0640d82e067b4a0e4
brayanrodbajo <bryan951112@gmail.com>
2016-01-06 01:11:16 -0500

    funciona lo del cliente lo crea y lo edita ... Falta pasar los checkbox cuando se actualiza la página para guardar la información del cliente y crear el pdf

--- SICON/ventas/urls.py
+++ SICON/ventas/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from .views import *
 
 urlpatterns = patterns('',
-	url(r'^ventas/cotizacion/vehiculos',cotizar_vehiculos, name='cotizacion_vehiculos')
-
+	url(r'^ventas/cotizacion/vehiculos',cotizar_vehiculos, name='cotizacion_vehiculos'),
+	url(r'^ventas/clientes/(\d+)$',gestionar_cliente, name='clientes')
 )
--- SICON/ventas/views_cotizacion_vehiculos.py
+++ SICON/ventas/views_cotizacion_vehiculos.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from administrador.models import VehiculoNuevo
+from models import Cliente
+from forms_cliente import ClienteForm
 
 def cotizar_vehiculos(request):
     vehiculos_n = VehiculoNuevo.objects.all()
     return render(request,'cotizacion_vehiculos.html',{'vehiculos_nuevos':vehiculos_n })
+
+def gestionar_cliente(request, identificacion):
+    vehiculos_n= VehiculoNuevo.objects.all()
+    cliente = Cliente.objects.filter(identificacion=identificacion).first()
+    print(cliente)
+    if cliente!=None:
+        form=ClienteForm(instance=cliente, initial=cliente.__dict__)
+        if request.method == 'POST':
+            form=ClienteForm(request.POST, instance=cliente, initial=cliente.__dict__)
+            if form.has_changed():
+                if form.is_valid():
+                    form.save()
+                    return HttpResponseRedirect("../cotizacion/vehiculos")
+    else:
+        form=ClienteForm()
+        if request.method == 'POST':
+            form=ClienteForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect("../cotizacion/vehiculos")
+    return render(request, 'cotizacion_vehiculos.html', {'vehiculos_nuevos': vehiculos_n, 'sig': True, 'form': form})

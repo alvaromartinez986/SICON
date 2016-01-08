ccb17a92a05e367a503e22e75b1f0be4ee4c39c7
brayanrodbajo <bryan951112@gmail.com>
2016-01-08 03:03:18 -0500

    FUNCIONANDO Y PROBADO

--- SICON/static/js/cotizacionVehiculos.js
+++ SICON/static/js/cotizacionVehiculos.js
@@ -6,51 +6,45 @@ function getCheckboxes() {
     var table = document.getElementById("vehiculos_nuevos");
     var checkboxes = table.getElementsByTagName("input");
     var carros = [];
-    var selected = 0;
+    var valorTotal=0;
     for (var i = 0; i < checkboxes.length; i++) {
         var checkbox = checkboxes[i];
         if(checkbox.checked){
             var currentRow = checkbox.parentNode.parentNode;
             var row = currentRow.getElementsByTagName("td");
-            selected++;
-            carros[selected]=row;
+            valorTotal+=parseInt(row[8].textContent);
+            var arrayRow = [row[2].textContent,row[3].textContent, row[4].textContent,row[5].textContent,row[6].textContent,row[7].textContent,row[8].textContent]
+            console.log(arrayRow);
+            carros.push(arrayRow);
         }
     }
+    carros.push(valorTotal);
     return carros;
 }
 
 function writeCotizacion(){
     var carros = getCheckboxes();
+    var valorTotal = carros[carros.length-1];
+    carros.length=carros.length-1;
     var identificacion  = $('body').data('identificacion');
     var ruta = "cotización"+ identificacion+".pdf";
-    var tabla = [];
-    var firstRow = [{ text: 'Cilindraje', bold: true }, { text: 'Marca', bold: true }, { text: 'Linea', bold: true }, { text: 'Modelo', bold: true }, { text: 'Tipo de Combustible', bold: true },{ text: 'Color', bold: true },{ text: 'Valor', bold: true }];
-    tabla.push(firstRow);
-    for (var i=0;i<carros.length;i++){
-        carros.length
-        var newRow=[];
-        console.log(carros);
-        for(var j=0;j<carros[i].length;j++){
-            if(j!==0&&j!==1) {
-                newRow.push(carros[i][j].textContent);
-            }
-        }
-        tabla.push(newRow);
+    var firstRow = [{ text: 'Cilindraje', bold: true }, { text: 'Marca', bold: true }, { text: 'Linea', bold: true }, { text: 'Modelo', bold: true }, { text: 'Tipo de Combustible', bold: true },{ text: 'Color', bold: true },{ text: 'Valor (COP)', bold: true }];
+    var tabla= [firstRow];
+    for (carro in carros){
+        tabla.push(carros[carro]);
     }
+    console.log(tabla);
     var docDefinition = {
       content: [
-        'Esta es la cotizacion hecha al cliente '+identificacion+':',
+        'Esta es la cotizacion hecha al cliente con número de identificación: '+identificacion+':',
 
         {
           table: {
-            // headers are automatically repeated if the table spans over multiple pages
-            // you can declare how many rows should be treated as headers
-            headerRows: 1,
-            widths: [ '*', 'auto', 100, '*' ],
+            body:tabla
 
-            body: tabla
           }
-        }
+        },
+          {text:'El valor total de la cotización es: '+valorTotal+' COP', bold:true, alignment: 'right' }
       ]
     };
     pdfMake.createPdf(docDefinition).download(ruta);

a8a8c9a3d7d75899d9beeb252470b7b223ff4e42
brayanrodbajo <bryan951112@gmail.com>
2016-01-07 20:40:15 -0500

    Ya genera el PDF

--- SICON/static/js/cotizacionVehiculos.js
+++ SICON/static/js/cotizacionVehiculos.js
@@ -18,15 +18,27 @@ function getCheckboxes() {
     }
 }
 
-function askPath(){
-
-}
-
 function writeCotizacion(){
-    //ruta = askPath();
-    console.log($('body').data('identificacion'));
-    //var ruta = "holi.pdf";
-    //var docDefinition = { content: 'This is an sample PDF printed with pdfMake' };
-    //pdfMake.createPdf(docDefinition).download(ruta);
+    var identificacion  = $('body').data('identificacion');
+    var ruta = "cotización"+ identificacion+".pdf";
+    var docDefinition = {
+      content: [
+        {
+          table: {
+            // headers are automatically repeated if the table spans over multiple pages
+            // you can declare how many rows should be treated as headers
+            headerRows: 1,
+            widths: [ '*', 'auto', 100, '*' ],
+
+            body: [
+              [ 'First', 'Second', 'Third', 'The last one' ],
+              [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+              [ { text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Val 4' ]
+            ]
+          }
+        }
+      ]
+    };
+    pdfMake.createPdf(docDefinition).download(ruta);
 
 }
